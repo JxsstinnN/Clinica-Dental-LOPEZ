f4fe5c37d4eee08361f43e5627af41b884f5a80c
JxsstinnN <justininfotep@gmail.com>
2022-12-05 22:42:49 -0400

    f

--- JS/bin/custom.js
+++ JS/bin/custom.js
@@ -8,25 +8,7 @@
       jQuery('.counter-number').countTo();
     });
     
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-    var el = $(this).attr('href');
-    var elWrapped = $(el);
-    var header_height = $('.navbar').height();
 
-    scrollToDiv(elWrapped,header_height);
-    return false;
-
-    function scrollToDiv(element,navheight){
-      var offset = element.offset();
-      var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
-
-      $('body,html').animate({
-      scrollTop: totalScroll
-      }, 300);
-    }
-});
     
   })(window.jQuery);
 
