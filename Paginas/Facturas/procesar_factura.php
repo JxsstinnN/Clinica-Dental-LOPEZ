3ba23c9209d4a916dabd845e59d48194c3f9767f
JxsstinnN <justininfotep@gmail.com>
2022-12-07 22:44:46 -0400

    pileta e mielda
    asd

--- Paginas/Facturas/procesar_factura.php
+++ Paginas/Facturas/procesar_factura.php
@@ -34,86 +34,8 @@ if ($_GET) {
     <!-- Fin de las Liberias-->
     <link rel="stylesheet" href="../../CSS/Pag_Usuarios.css">
     <link rel="stylesheet" href="../../CSS/Tablas_Medic.css">
+    <link rel="stylesheet" href="../../CSS/Factura.css">
     <title>Factura</title>
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-
-        body {
-            background-color: #f1f1f1;
-        }
-
-        #regForm {
-            background-color: #ffffff;
-            margin: 100px auto;
-            font-family: Raleway;
-            padding: 40px;
-            width: 70%;
-            min-width: 300px;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        input {
-            padding: 10px;
-            width: 100%;
-            font-size: 17px;
-            font-family: Raleway;
-            border: 1px solid #aaaaaa;
-        }
-
-        /* Mark input boxes that gets an error on validation: */
-        input.invalid {
-            background-color: #ffdddd;
-        }
-
-        /* Hide all steps by default: */
-        .tab {
-            display: none;
-        }
-
-        button {
-            background-color: #04AA6D;
-            color: #ffffff;
-            border: none;
-            padding: 10px 20px;
-            font-size: 17px;
-            font-family: Raleway;
-            cursor: pointer;
-        }
-
-        button:hover {
-            opacity: 0.8;
-        }
-
-        #prevBtn {
-            background-color: #bbbbbb;
-        }
-
-        /* Make circles that indicate the steps of the form: */
-        .step {
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #bbbbbb;
-            border: none;
-            border-radius: 50%;
-            display: inline-block;
-            opacity: 0.5;
-        }
-
-        .step.active {
-            opacity: 1;
-        }
-
-        /* Mark the steps that are finished and valid: */
-        .step.finish {
-            background-color: #04AA6D;
-        }
-    </style>
 </head>
 
 <body>
@@ -197,15 +119,23 @@ if ($_GET) {
         <form action="factura_final.php" method="post" id="FormFac"></form>
         <h1>Facturación:</h1>
         <div class="tab">Datos del cliente:
-            <p><input oninput="this.className = ''" value="<?php echo $nombre_paciente ?>"></p>
-            <p><input oninput="this.className = ''" value="<?php echo $ape_paciente ?>"></p>
-            <p><input oninput=" this.className=''" value=" <?php echo $titulo; ?>"></p>
+            <p class="inf">Nombre<input class="text" value="<?php echo $nombre_paciente ?>" readonly></p>
+            <p class="inf">Apellido<input class="text" value="<?php echo $ape_paciente ?>" readonly></p>
+            <p class="inf">Servicio Realizado<input class="text" value=" <?php echo $titulo; ?>" readonly></p>
         </div>
 
         <div class="tab">Datos de pago:
-            <p><input placeholder="Costo del servicio" oninput="this.className = ''" value="<?php echo $costo ?>"></p>
-
+            <p class="inf"><input class="text" name="costo_serv" placeholder="Costo del servicio" value="<?php echo $costo ?>" readonly></p>
+            <p class="inf">Tipo de Pago</p>
+            <a href="#efe">
+                <p class="inf">Efectivo<input type="radio" name="Efectivo" id="Efectivo"></p>
+            </a><br>
+            <a href="#tar">
+                <p class="inf">Tarjeta<input type="radio" name="Efectivo" id="Tarjeta"></p>
+            </a>
         </div>
+
+
 
         <div style="overflow:auto;">
             <div style="float:right;">
@@ -220,6 +150,59 @@ if ($_GET) {
             <span class="step"></span>
             <span class="step"></span>
             <span class="step"></span>
+        </div>
+
+
+        <div id="efe" class="overlay1">
+            <div class="popupc">
+                <h2>Efectivo</h2>
+                <a class="close" href="#">&times;</a>
+                <div class="content">
+                    RD$<p class="inf"><input class="text" placeholder="Costo del servicio" value="<?php echo $costo ?>" readonly></p>
+                    RD$<p class="inf"><input class="text" placeholder="Monto Recibido" name="recibido_cliente"></p>
+
+                </div>
+            </div>
+        </div>
+
+        <div id="tar" class="overlay1">
+            <div class="popupc">
+                <h2>Tarjeta</h2>
+                <a class="close" href="#">&times;</a>
+                <div class="content">
+                    <div class="tarj-div">
+                        <h3>Payment</h3>
+                        <label for="fname">Accepted Cards</label>
+                        <div class="icon-container">
+                            <i class="fa fa-cc-visa" style="color:navy;"></i>
+                            <i class="fa fa-cc-amex" style="color:blue;"></i>
+                            <i class="fa fa-cc-mastercard" style="color:red;"></i>
+                            <i class="fa fa-cc-discover" style="color:orange;"></i>
+                        </div>
+                        <label for="cname">Name on Card</label>
+                        <input type="text" id="cname" name="cardname" placeholder="John More Doe">
+                        <br>
+                        <label for="ccnum">Credit card number</label>
+                        <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+                        <br>
+                        <label for="expmonth">Exp Month</label>
+                        <input type="text" id="expmonth" name="expmonth" placeholder="September">
+
+                        <div class="row">
+                            <div class="col-50">
+                                <label for="expyear">Exp Year</label>
+                                <input type="text" id="expyear" name="expyear" placeholder="2018">
+                            </div>
+                            <div class="col-50">
+                                <label for="cvv">CVV</label>
+                                <input type="text" id="cvv" name="cvv" placeholder="352">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
         </div>
 
 
@@ -250,7 +233,6 @@ if ($_GET) {
                 // This function will figure out which tab to display
                 var x = document.getElementsByClassName("tab");
                 // Exit the function if any field in the current tab is invalid:
-                if (n == 1 && !validateForm()) return false;
                 // Hide the current tab:
                 x[currentTab].style.display = "none";
                 // Increase or decrease the current tab by 1:
@@ -265,27 +247,6 @@ if ($_GET) {
                 showTab(currentTab);
             }
 
-            function validateForm() {
-                // This function deals with validation of the form fields
-                var x, y, i, valid = true;
-                x = document.getElementsByClassName("tab");
-                y = x[currentTab].getElementsByTagName("input");
-                // A loop that checks every input field in the current tab:
-                for (i = 0; i < y.length; i++) {
-                    // If a field is empty...
-                    if (y[i].value == "") {
-                        // add an "invalid" class to the field:
-                        y[i].className += " invalid";
-                        // and set the current valid status to false:
-                        valid = false;
-                    }
-                }
-                // If the valid status is true, mark the step as finished and valid:
-                if (valid) {
-                    document.getElementsByClassName("step")[currentTab].className += " finish";
-                }
-                return valid; // return the valid status
-            }
 
             function fixStepIndicator(n) {
                 // This function removes the "active" class of all steps...
