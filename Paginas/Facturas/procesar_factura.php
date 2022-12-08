7eff2b32cba5fd01150af4ef83568f8a0b2c534e
JxsstinnN <justininfotep@gmail.com>
2022-12-07 21:35:06 -0400

    asd

--- Paginas/Facturas/procesar_factura.php
+++ Paginas/Facturas/procesar_factura.php
@@ -2,14 +2,17 @@
 session_start();
 require_once "../../Clases/config.php";
 
-if ($_POST) {
-    $id = $_POST['id'];
-    $action = $_POST['action'];
+if ($_GET) {
+    $id = $_GET['id'];
+    $action = $_GET['action'];
     if ($action == 'fac') {
-        $query = $con->prepare("select * from citas  WHERE ID_CITA ='$id'");
+        $query = $con->prepare("select * from citas join medico on Citas.ID_Medico = medico.ID_Medico JOIN pacientes on pacientes.ID_Paciente = citas.ID_Paciente JOIN usuario on usuario.User_ID = citas.User_ID WHERE ID_CITA ='$id'");
         $query->execute();
-        $result = $query->fetchALL(PDO::FETCH_ASSOC);;
-        print_r($result);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        $nombre_paciente = $result['Nombre_Paciente'];
+        $ape_paciente = $result['Apellido_Paciente'];
+        $titulo = $result['Titulo'];
+        $costo = $result['Costo'];
     }
 }
 ?>
@@ -21,17 +24,280 @@ if ($_POST) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Procesar_Factura</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <!-- Liberias-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+    <!-- Fin de las Liberias-->
+    <link rel="stylesheet" href="../../CSS/Pag_Usuarios.css">
+    <link rel="stylesheet" href="../../CSS/Tablas_Medic.css">
+    <title>Factura</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        body {
+            background-color: #f1f1f1;
+        }
+
+        #regForm {
+            background-color: #ffffff;
+            margin: 100px auto;
+            font-family: Raleway;
+            padding: 40px;
+            width: 70%;
+            min-width: 300px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        input {
+            padding: 10px;
+            width: 100%;
+            font-size: 17px;
+            font-family: Raleway;
+            border: 1px solid #aaaaaa;
+        }
+
+        /* Mark input boxes that gets an error on validation: */
+        input.invalid {
+            background-color: #ffdddd;
+        }
+
+        /* Hide all steps by default: */
+        .tab {
+            display: none;
+        }
+
+        button {
+            background-color: #04AA6D;
+            color: #ffffff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 17px;
+            font-family: Raleway;
+            cursor: pointer;
+        }
+
+        button:hover {
+            opacity: 0.8;
+        }
+
+        #prevBtn {
+            background-color: #bbbbbb;
+        }
+
+        /* Make circles that indicate the steps of the form: */
+        .step {
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbbbbb;
+            border: none;
+            border-radius: 50%;
+            display: inline-block;
+            opacity: 0.5;
+        }
+
+        .step.active {
+            opacity: 1;
+        }
+
+        /* Mark the steps that are finished and valid: */
+        .step.finish {
+            background-color: #04AA6D;
+        }
+    </style>
 </head>
+
 <body>
 
-    <div class="tab">Datos del cliente:
-        <p><input placeholder="Nombre del paciente" oninput="this.className = ''" value=""></p>
-        <p><input placeholder="Last name..." oninput="this.className = ''"></p>
+    <header>
+        <div class="icon__menu">
+            <i class="fas fa-bars" id="btn_open"></i>
+        </div>
+
+        <div id="current-date"> <span>Fecha Actual:</span>
+            <div class="right-date" id="fecha">AAAAAAAAAAA</div>
+        </div>
+
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $_SESSION['nombre']; ?>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="../../PHP/Sesion.php?sesion=cerrar">Cerrar sesion </a>
+            </div>
+        </div>
+    </header>
+
+    <div class="menu__side" id="menu_side">
+
+        <div class="name__page">
+            <i class="fa-solid fa-hospital"></i>
+            <h4>Clinica Grullón</h4>
+        </div>
+
+        <div class="options__menu">
+
+            <a href="../Administrativo/Pag_usuario.php">
+                <div class="option">
+                    <i class="fas fa-home" title="Inicio"></i>
+                    <h4>Inicio</h4>
+                </div>
+            </a>
+
+            <a href="../Administrativo/Doctores_CRUD.php">
+                <div class="option">
+                    <i class="fa-solid fa-user-doctor" title="Doctores"></i>
+                    <h4>Doctores</h4>
+                </div>
+            </a>
+
+            <a href="../Administrativo/Pacientes_CRUD.php">
+                <div class="option">
+                    <i class='bx bx-group' title="Pacientes"></i>
+                    <h4>Pacientes</h4>
+                </div>
+            </a>
+
+            <a href="../Pacientes/Citas.php" class="selected">
+                <div class="option">
+                    <i class="fa-solid fa-calendar-check"></i>
+                    <h4>Citas</h4>
+                </div>
+            </a>
+
+            <?php
+            if ($_SESSION["tipo_usuario"] == "ADMIN") {
+                echo "<a href='../config/config.php'>
+                <div class='option'>
+                    <i class='bx bx-cog' title='Contacto'></i>
+                    <h4>Configuración</h4>
+                </div>
+            </a>";
+            }
+
+
+            ?>
+
+
+        </div>
+
     </div>
 
 
+    <main>
+        <form action="factura_final.php" method="post" id="FormFac"></form>
+        <h1>Facturación:</h1>
+        <div class="tab">Datos del cliente:
+            <p><input oninput="this.className = ''" value="<?php echo $nombre_paciente ?>"></p>
+            <p><input oninput="this.className = ''" value="<?php echo $ape_paciente ?>"></p>
+            <p><input oninput=" this.className=''" value=" <?php echo $titulo; ?>"></p>
+        </div>
 
+        <div class="tab">Datos de pago:
+            <p><input placeholder="Costo del servicio" oninput="this.className = ''" value="<?php echo $costo ?>"></p>
+
+        </div>
+
+        <div style="overflow:auto;">
+            <div style="float:right;">
+                <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+                <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+            </div>
+        </div>
+
+        <!-- Circles which indicates the steps of the form: -->
+        <div style="text-align:center;margin-top:40px;">
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+        </div>
+
+
+        <script>
+            var currentTab = 0; // Current tab is set to be the first tab (0)
+            showTab(currentTab); // Display the current tab
+
+            function showTab(n) {
+                // This function will display the specified tab of the form ...
+                var x = document.getElementsByClassName("tab");
+                x[n].style.display = "block";
+                // ... and fix the Previous/Next buttons:
+                if (n == 0) {
+                    document.getElementById("prevBtn").style.display = "none";
+                } else {
+                    document.getElementById("prevBtn").style.display = "inline";
+                }
+                if (n == (x.length - 1)) {
+                    document.getElementById("nextBtn").innerHTML = "Submit";
+                } else {
+                    document.getElementById("nextBtn").innerHTML = "Next";
+                }
+                // ... and run a function that displays the correct step indicator:
+                fixStepIndicator(n)
+            }
+
+            function nextPrev(n) {
+                // This function will figure out which tab to display
+                var x = document.getElementsByClassName("tab");
+                // Exit the function if any field in the current tab is invalid:
+                if (n == 1 && !validateForm()) return false;
+                // Hide the current tab:
+                x[currentTab].style.display = "none";
+                // Increase or decrease the current tab by 1:
+                currentTab = currentTab + n;
+                // if you have reached the end of the form... :
+                if (currentTab >= x.length) {
+                    //...the form gets submitted:
+                    document.getElementById("regForm").submit();
+                    return false;
+                }
+                // Otherwise, display the correct tab:
+                showTab(currentTab);
+            }
+
+            function validateForm() {
+                // This function deals with validation of the form fields
+                var x, y, i, valid = true;
+                x = document.getElementsByClassName("tab");
+                y = x[currentTab].getElementsByTagName("input");
+                // A loop that checks every input field in the current tab:
+                for (i = 0; i < y.length; i++) {
+                    // If a field is empty...
+                    if (y[i].value == "") {
+                        // add an "invalid" class to the field:
+                        y[i].className += " invalid";
+                        // and set the current valid status to false:
+                        valid = false;
+                    }
+                }
+                // If the valid status is true, mark the step as finished and valid:
+                if (valid) {
+                    document.getElementsByClassName("step")[currentTab].className += " finish";
+                }
+                return valid; // return the valid status
+            }
+
+            function fixStepIndicator(n) {
+                // This function removes the "active" class of all steps...
+                var i, x = document.getElementsByClassName("step");
+                for (i = 0; i < x.length; i++) {
+                    x[i].className = x[i].className.replace(" active", "");
+                }
+                //... and adds the "active" class to the current step:
+                x[n].className += " active";
+            }
+        </script>
+    </main>
 </body>
 
 </html>
